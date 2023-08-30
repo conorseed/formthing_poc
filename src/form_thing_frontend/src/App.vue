cfd7f935ab6a0e2bb38541bae50d1c7408d6c0ec
Conor <13262289+conorseed@users.noreply.github.com>
2023-08-30 20:36:08 +1200

    demo notification

--- src/form_thing_frontend/src/App.vue
+++ src/form_thing_frontend/src/App.vue
@@ -1,4 +1,10 @@
 <template>
+  <div class="text-bold text-red-80 bg-red-300 px-6 py-2 text-center text-sm">
+    This is a demo only. Do not use this in production or for sensitive data.
+    <a class="underline" target="_blank" href="https://github.com/conorseed/formthing_poc"
+      >More info.</a
+    >
+  </div>
   <RouterView />
 </template>
 
