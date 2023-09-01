76df86b98f3669996ac4dd31b741482b7e560633
Conor <13262289+conorseed@users.noreply.github.com>
2023-09-01 20:52:07 +1200

    Header mobile nav

--- src/form_thing_frontend/src/components/TheHeader.vue
+++ src/form_thing_frontend/src/components/TheHeader.vue
@@ -9,7 +9,7 @@
           Form<span class="text-indigo-400">Thing</span>
         </RouterLink>
       </div>
-      <div v-if="authStore.actor" class="hidden justify-center md:flex">
+      <div v-if="authStore.actor" class="hidden justify-center lg:flex">
         <div class="ml-10 flex items-center space-x-4">
           <RouterLink
             v-for="item in navigation"
@@ -33,7 +33,7 @@
           >
         </div>
       </div>
-      <div class="flex justify-end">
+      <div class="hidden justify-end lg:flex">
         <button
           v-if="!authStore.actor"
           @click="login"
@@ -90,15 +90,105 @@
           </transition>
         </Popover>
       </div>
+      <!--MOBILE MENU-->
+      <div class="flex justify-end lg:hidden">
+        <button
+          type="button"
+          class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white lg:hidden"
+          @click="mobileMenuOpen = true"
+        >
+          <span class="sr-only">Open main menu</span>
+          <Bars3Icon class="h-6 w-6" aria-hidden="true" />
+        </button>
+        <Dialog as="div" class="" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
+          <div class="fixed inset-0 z-10" />
+          <DialogPanel
+            class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-indigo-950 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
+          >
+            <div class="flex items-center justify-between">
+              <RouterLink
+                :to="{ name: 'home' }"
+                @click="mobileMenuOpen = false"
+                class="-m-1.5 p-1.5 font-bold text-white"
+              >
+                Form<span class="text-indigo-400">Thing</span>
+              </RouterLink>
+              <button
+                type="button"
+                class="-m-2.5 rounded-md p-2.5 text-white"
+                @click="mobileMenuOpen = false"
+              >
+                <span class="sr-only">Close menu</span>
+                <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+              </button>
+            </div>
+            <div class="mt-6 flow-root">
+              <div class="-my-6 divide-y divide-indigo-900">
+                <div class="space-y-2 py-6">
+                  <RouterLink
+                    v-for="item in navigation"
+                    :key="item.name"
+                    :to="item.to"
+                    @click="mobileMenuOpen = false"
+                    :class="[
+                      router.currentRoute.value.name == item.to.name
+                        ? 'text-white underline underline-offset-8'
+                        : 'text-gray-400 hover:text-white hover:underline hover:underline-offset-8',
+                      'group flex gap-2 rounded-md py-4 text-sm font-medium transition-all'
+                    ]"
+                    :aria-current="
+                      router.currentRoute.value.name == item.to.name ? 'page' : undefined
+                    "
+                  >
+                    <component
+                      v-if="item.icon"
+                      :is="item.icon"
+                      class="h-5 w-5 flex-none"
+                      aria-hidden="true"
+                    />
+                    {{ item.name }}</RouterLink
+                  >
+                </div>
+                <div class="py-6">
+                  <button
+                    v-if="!authStore.actor"
+                    @click="login"
+                    class="px-3 py-2 text-sm font-semibold leading-6 text-white"
+                  >
+                    Log in <span aria-hidden="true">&rarr;</span>
+                  </button>
+                  <template v-if="authStore.principal">
+                    <button
+                      @click="copy(authStore.principal.toString(), $event)"
+                      type="button"
+                      class="group flex gap-2 text-left text-white"
+                    >
+                      <DocumentDuplicateIcon v-if="!copied" class="h-6 w-6" />
+                      <CheckIcon v-else class="h-6 w-6 text-green-500" />
+                      <span class="text-xs">{{ authStore.principal?.toString() }}</span>
+                    </button>
+                    <button
+                      @click="logout"
+                      class="mt-4 px-3 py-4 text-sm font-semibold leading-6 text-white"
+                    >
+                      Log out <span aria-hidden="true">&rarr;</span>
+                    </button>
+                  </template>
+                </div>
+              </div>
+            </div>
+          </DialogPanel>
+        </Dialog>
+      </div>
     </nav>
   </header>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
-import { Popover, PopoverButton, PopoverPanel } from '@headlessui/vue'
+import { Popover, PopoverButton, PopoverPanel, DialogPanel, Dialog } from '@headlessui/vue'
 import { ChevronDownIcon, PlusIcon } from '@heroicons/vue/20/solid'
-import { DocumentDuplicateIcon, CheckIcon } from '@heroicons/vue/24/outline'
+import { DocumentDuplicateIcon, CheckIcon, XMarkIcon, Bars3Icon } from '@heroicons/vue/24/outline'
 import { useAuthStore } from '@/stores/authStore'
 import { useNotificationStore } from '@/stores/useNotificationStore'
 import { useRouter } from 'vue-router'
@@ -119,6 +209,8 @@ const navigation = [
     icon: PlusIcon
   }
 ]
+
+const mobileMenuOpen = ref(false)
 
 const copied = ref(false)
 const copy = (input: string, e: Event) => {
@@ -153,6 +245,7 @@ const login = async (e: Event) => {
       status: 'error'
     })
   }
+  mobileMenuOpen.value = false
 }
 
 const logout = async (e: Event) => {
@@ -175,5 +268,6 @@ const logout = async (e: Event) => {
       status: 'error'
     })
   }
+  mobileMenuOpen.value = false
 }
 </script>
